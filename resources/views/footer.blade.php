126e3b2342ef491ec06d53371e14e7468f34108a
Citrom2021 <citronejoe0123@citromail.hu>
2022-03-14 22:00:52 +0100

    Auth Finale+ view issues for HMS

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,110 @@
+<footer class="ftco-footer ftco-bg-dark ftco-section">
+      <div class="container">
+        <div class="row mb-5">
+          <div class="col-md">
+            <div class="ftco-footer-widget mb-4">
+              <h2 class="ftco-heading-2">Budapest Hotel</h2>
+              <p>Lórum ipse mint virált foszfok glasz, elsősorban egy hező kíség. A közésként tános jegég hajogaságait, szintéseit elődi a majás roslata. Ennek közvetlen órsára volt a fiztó töldségen csörös ködő az edveles göresekről.</p>
+              <ul class="ftco-footer-social list-unstyled float-md-left float-lft mt-5">
+                <li class="ftco-animate"><a href="#"><span class="icon-twitter"></span></a></li>
+                <li class="ftco-animate"><a href="#"><span class="icon-facebook"></span></a></li>
+                <li class="ftco-animate"><a href="#"><span class="icon-instagram"></span></a></li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-md">
+            <div class="ftco-footer-widget mb-4 ml-md-5">
+              <h2 class="ftco-heading-2">Useful Links</h2>
+              <ul class="list-unstyled">
+                <li><a href="#" class="py-2 d-block">Blog</a></li>
+                <li><a href="#" class="py-2 d-block">Rooms</a></li>
+                <li><a href="#" class="py-2 d-block">Amenities</a></li>
+                <li><a href="#" class="py-2 d-block">Gift Card</a></li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-md">
+             <div class="ftco-footer-widget mb-4">
+              <h2 class="ftco-heading-2">Privacy</h2>
+              <ul class="list-unstyled">
+                <li><a href="#" class="py-2 d-block">Career</a></li>
+                <li><a href="#" class="py-2 d-block">About Us</a></li>
+                <li><a href="#" class="py-2 d-block">Contact Us</a></li>
+                <li><a href="#" class="py-2 d-block">Services</a></li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-md">
+            <div class="ftco-footer-widget mb-4">
+            	<h2 class="ftco-heading-2">Have a Questions?</h2>
+            	<div class="block-23 mb-3">
+	              <ul>
+	                <li><span class="icon icon-map-marker"></span><span class="text">1012 Budapest, Képzelet utca 66, Hungary</span></li>
+	                <li><a href="#"><span class="icon icon-phone"></span><span class="text">+36 392 3929 210</span></a></li>
+	                <li><a href="#"><span class="icon icon-envelope"></span><span class="text">info@budapesthotel.com</span></a></li>
+	              </ul>
+	            </div>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-12 text-center">
+
+            <p>
+  Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | Budapest Hotel</a>
+</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+    
+  
+
+  <!-- loader -->
+  <div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00"/></svg></div>
+
+<!--
+  <script src="js/jquery.min.js"></script>
+  <script src="js/jquery-migrate-3.0.1.min.js"></script>
+  <script src="js/popper.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/jquery.easing.1.3.js"></script>
+  <script src="js/jquery.waypoints.min.js"></script>
+  <script src="js/jquery.stellar.min.js"></script>
+  <script src="js/owl.carousel.min.js"></script>
+  <script src="js/jquery.magnific-popup.min.js"></script>
+  <script src="js/aos.js"></script>
+  <script src="js/jquery.animateNumber.min.js"></script>
+  <script src="js/jquery.mb.YTPlayer.min.js"></script>
+  <script src="js/bootstrap-datepicker.js"></script>
+  <script src="js/jquery.timepicker.min.js"></script> 
+  <script src="js/scrollax.min.js"></script>
+  
+  <script src="js/google-map.js"></script>
+  <script src="js/main.js"></script> -->
+
+  <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false"></script>
+  <script src="{{URL::asset('assets/hms2js/javascripts/aos.js')}}" type="text/javascript">	</script>
+	<script src="{{URL::asset('assets/hms2js/javascripts/bootstrap-datepicker.js')}}" type="text/javascript">	</script>
+	<script src="{{URL::asset('assets/hms2js/javascripts/bootstrap.js')}}" type="text/javascript"></script>
+	<script src="{{URL::asset('assets/hms2js/javascripts/bootstrap.min.js')}}" type="text/javascript"></script>
+	<script src="{{URL::asset('assets/hms2js/javascripts/button.js')}}" type="text/javascript"></script>
+	<script src="{{URL::asset('assets/hms2js/javascripts/dataTables.bootstrap.js')}}" type="text/javascript">	</script>
+	<script src="{{URL::asset('assets/hms2js/javascripts/google-map.js')}}" type="text/javascript">	</script>
+	<script src="{{URL::asset('assets/hms2js/javascripts/jquery-3.2.1.min.js')}}" type="text/javascript">	</script>
+	<script src="{{URL::asset('assets/hms2js/javascripts/jquery-migrate-3.0.1.min.js')}}" type="text/javascript">	</script>
+	<script src="{{URL::asset('assets/hms2js/javascripts/jquery.animateNumber.min.js')}}" type="text/javascript">	</script>
+	<script src="{{URL::asset('assets/hms2js/javascripts/jquery.dataTables.js')}}" type="text/javascript"></script>
+	<script src="{{URL::asset('assets/hms2js/javascripts/jquery.easing.1.3.js')}}" type="text/javascript"></script>
+	<script src="{{URL::asset('assets/hms2js/javascripts/jquery.js')}}" type="text/javascript"></script>
+	<script src="{{URL::asset('assets/hms2js/javascripts/jquery.magnific-popup.min.js')}}" type="text/javascript"></script>
+	<script src="{{URL::asset('assets/hms2js/javascripts/jquery.min.js')}}" type="text/javascript"></script>
+	<script src="{{URL::asset('assets/hms2js/javascripts/jquery.stellar.min.js')}}" type="text/javascript"></script>
+	<script src="{{URL::asset('assets/hms2js/javascripts/jquery.waypoints.min.js')}}" type="text/javascript"></script>
+	<script src="{{URL::asset('assets/hms2js/javascripts/main.js')}}" type="text/javascript"></script>
+	<script src="{{URL::asset('assets/hms2js/javascripts/owl.carousel.min.js')}}" type="text/javascript"></script>
+	<script src="{{URL::asset('assets/hms2js/javascripts/popper.min.js')}}" type="text/javascript"></script>
+	<script src="{{URL::asset('assets/hms2js/javascripts/range.js')}}" type="text/javascript"></script>
+	<script src="{{URL::asset('assets/hms2js/javascripts/scrollax.min.js')}}" type="text/javascript"></script>
+  </body>
+</html>
