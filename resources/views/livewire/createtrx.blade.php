f133e2c3899c7a3254b1a43b0bda52ef2b358dd5
Citrom2021 <citronejoe0123@citromail.hu>
2022-06-13 02:17:25 +0200

    make it heroku

--- resources/views/livewire/createtrx.blade.php
+++ resources/views/livewire/createtrx.blade.php
@@ -115,7 +115,7 @@
                         </div>
                         <div class="mb-4">
                             <label for="number_of_guests" class="block text-gray-700 text-sm font-bold mb-2">Number of guests:</label>
-                            <input required type="number" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="number_of_guests" wire:model="number_of_guests">
+                            <input required type="text" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="number_of_guests" wire:model="number_of_guests">
                         </div>
 
                         <div class="bg-gray-50 px-4 py-3 sm:px-6 sm:flex  sm:flex-col space-y-4">
